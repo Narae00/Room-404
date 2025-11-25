a2d3050c53a7a5f4160dea72021d4fc12cef8252
Kimjh <cain981116@gmail.com>
2025-11-26 03:39:46 +0900

    화장실 퍼즐 수정

---
+++ Assets/Room404-assets/Scipts/KnobWaterActivator.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Content.Interaction;
+
+public class KnobWaterActivator : MonoBehaviour
+{
+    public XRKnob knob;                         // XRKnob 참조
+    public FaucetWaterController waterControl;  // 물 스크립트
+
+    public float triggerValue = 1f;             // value가 이 이상이면 물 켜짐
+
+    private void Start()
+    {
+        // XRKnob의 값 변경 이벤트 연결
+        knob.onValueChange.AddListener(OnKnobValueChanged);
+    }
+
+    private void OnKnobValueChanged(float val)
+    {
+        // 값이 충분히 올라가면 물 ON
+        if (val >= triggerValue)
+        {
+            waterControl.StartWater();
+        }
+        else
+        {
+            waterControl.StopWater();
+        }
+    }
+}
