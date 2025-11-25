9a69d864f47044051513fc3353cb16a6576dcec3
Kimjh <cain981116@gmail.com>
2025-11-25 22:40:17 +0900

    핸드 텔레포트 스크립트 수정

--- Assets/My Assets/Animated Hands/Scripts/TeleportationActivator.cs
+++ Assets/My Assets/Animated Hands/Scripts/TeleportationActivator.cs
@@ -1,25 +1,33 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
-public class TeleportationAcrivator : MonoBehaviour
+public class TeleportationActivator : MonoBehaviour
 {
     public XRRayInteractor teleportInteractor;
+    public XRRayInteractor rayInteractor;
     public InputActionProperty telepotActivatorAction;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         teleportInteractor.gameObject.SetActive(false);
-
         telepotActivatorAction.action.performed += Action_performed;
+        rayInteractor.uiHoverEntered.AddListener(x => DisableTeleportRay());
     }
 
     private void Action_performed(InputAction.CallbackContext obj)
     {
+        if (rayInteractor && rayInteractor.IsOverUIGameObject())
+        {
+            return ;
+        }
         teleportInteractor.gameObject.SetActive(true);
     }
 
-    // Update is called once per frame
+    public void DisableTeleportRay()
+    {
+        teleportInteractor.gameObject.SetActive(false);
+    }
+
     void Update()
     {
         if (telepotActivatorAction.action.WasReleasedThisFrame())
